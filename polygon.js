6f6d45a14379194dbbb18532d01d045a3a2da56b
Hrik Sinha <dailyhrik@gmail.com>
2023-09-05 21:03:55 +0530

    adding conditions if/else

--- polygon.js
+++ polygon.js
@@ -1,18 +1,48 @@
-var inputs = document.querySelectorAll(".angle-inputs");
+var inputs = document.querySelectorAll(".angle-input");
 var checkButton = document.querySelector("#check-btn");
 var outputResult = document.querySelector("#output");
 
-checkButton.addEventListener("click", knowPolygon);
+checkButton.addEventListener("click", knowYourPolygon);
 
 
 function calculateSumOfAngles(angle1,angle2,angle3,angle4,angle5,angle6,angle7,angle8,angle9,angle10) {
 
     var sumOfAngles = angle1 + angle2 + angle3 + angle4 + angle5 + angle6 + angle7 + angle8 + angle9 + angle10 ;
-    console.log(sumOfAngles);
+    return sumOfAngles;
 
 }
 
 
-function knowPolygon() {
-    console.log("Checking");
+function knowYourPolygon() {
+
+
+    var sumOfAngles = calculateSumOfAngles (Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value), Number(inputs[3].value), Number(inputs[4].value), Number(inputs[5].value), Number(inputs[6].value), Number(inputs[7].value), Number(inputs[8].value), Number(inputs[9].value) )
+    if(sumOfAngles === 1440){
+        console.log("Sum of all these angles will form Decagon");
+    } else if ( sumOfAngles ===1260){
+        console.log("Sum of all these angles will form Nonagon");
+
+    } else if( sumOfAngles ===1080){
+        console.log("Sum of all these angles will form Octagon");
+
+    } else if( sumOfAngles ===900){
+        console.log("Sum of all these angles will form Heptagon");
+
+    } else if( sumOfAngles === 720){
+        console.log("Sum of all these angles will form Hexagon");
+ 
+    } else if ( sumOfAngles === 540){
+        console.log("Sum of all these angles will form Pentagon");
+
+    } else if(sumOfAngles === 360){
+        console.log("Sum of all these angles will form Quadrilateral");
+
+    } else if (sumOfAngles === 180){
+        console.log("Sum of all these angles will form Triangle");
+
+    } else {
+        console.log("Too small to calculate");
+    }
+    
 }
+
